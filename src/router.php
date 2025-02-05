e62241abd9c4f9ef452db4acfd96862903ed690f
MaSo1996 <138728469+MaSo1996@users.noreply.github.com>
2025-02-05 21:51:45 +0100

    New section from course

---
+++ src/router.php
@@ -0,0 +1,19 @@
+<?php
+
+class Router
+{
+  private array $routes = [];
+
+  public function add(string $path, array $params) : void
+  {
+    $this->routes[] = [
+      "path" => $path,
+      "params" => $params
+    ];
+  }
+
+  public function match()
+  {
+    
+  }
+}
