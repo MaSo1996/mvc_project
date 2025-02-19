cf2af5ce775cd254278ea99b3c2a718c64b80c64
MaSo1996 <138728469+MaSo1996@users.noreply.github.com>
2025-02-19 22:24:40 +0100

    New section

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -7,30 +7,27 @@ use Framework\Viewer;
 
 class Products
 {
+
+  public function __construct(private Viewer $viewer, private Product $model) {}
+
   public function index()
   {
-    $model = new \App\Models\Product;
+    $products = $this->model->getData();
 
-    $products = $model->getData();
-
-    $viewer = new Viewer;
-
-    echo $viewer->render('shared/header.php', [
+    echo $this->viewer->render('shared/header.php', [
       'title' => 'Products'
     ]);
 
-    echo $viewer->render("Products/index.php", ['products' => $products]);
+    echo $this->viewer->render("Products/index.php", ['products' => $products]);
   }
 
   public function show(string $id)
   {
-    $viewer = new Viewer;
-
-    echo $viewer->render('shared/header.php', [
+    echo $this->viewer->render('shared/header.php', [
       'title' => 'Product'
     ]);
 
-    echo $viewer->render("Products/show.php", ['id' => $id]);
+    echo $this->viewer->render("Products/show.php", ['id' => $id]);
   }
 
   public function showPage(string $title, string $id, string $page)
